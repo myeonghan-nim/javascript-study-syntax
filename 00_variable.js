3612b487d083914f48b5bd460b1b1796a42ba66f
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-10-29 11:25:47 +0900

    Add: variables

---
+++ 00_variable.js
@@ -0,0 +1,28 @@
+// characteristics of let
+// let x = 1
+// let x = 2  // let cannot define same variable
+
+// x = 3
+
+// console.log(x)
+
+
+// block-scope
+// let x = 1
+
+// if (x === 1) {
+//   let x = 2
+//   console.log(x)  // variables can alive in block-scope
+// }
+// console.log(x)
+
+
+// diff of variable types
+let x  // let only define variable
+const y = 9  // const define variable and value
+
+if (y === 9) {
+  const y = 10
+  console.log(y)
+}
+console.log(y)
