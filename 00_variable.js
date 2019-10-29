d749cff484437b7d2e38c99a6b360b3683860dcd
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-10-29 11:28:22 +0900

    Change: variables

--- 00_variable.js
+++ 00_variable.js
@@ -1,23 +1,28 @@
 // characteristics of let
-// let x = 1
-// let x = 2  // let cannot define same variable
+/*
+let x = 1
+let x = 2  // let cannot define same variable
 
-// x = 3
+x = 3
 
-// console.log(x)
+console.log(x)
+*/
 
 
 // block-scope
-// let x = 1
+/*
+let x = 1
 
-// if (x === 1) {
-//   let x = 2
-//   console.log(x)  // variables can alive in block-scope
-// }
-// console.log(x)
+if (x === 1) {
+  let x = 2
+  console.log(x)  // variables can alive in block-scope
+}
+console.log(x)
+*/
 
 
 // diff of variable types
+/*
 let x  // let only define variable
 const y = 9  // const define variable and value
 
@@ -26,3 +31,4 @@ if (y === 9) {
   console.log(y)
 }
 console.log(y)
+*/
