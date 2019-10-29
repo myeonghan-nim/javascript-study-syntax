748d361de3d832818214c8e57871b8e48f2e8d7d
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-10-29 13:50:13 +0900

    Add: operator

---
+++ 01_operator.js
@@ -0,0 +1,30 @@
+// +, -, *, / operation
+let a = 0
+
+a = a + 3
+console.log(a)
+a += 3
+console.log(a)
+a++
+console.log(a)
+
+
+// compare operation
+console.log(3 < 2)
+console.log('a' < 'b')
+console.log('a' < 'A')
+
+console.log(1 == '1')
+console.log(1 === '1')
+console.log(1 === Number('1'))
+
+console.log(true && false)  // and
+console.log(true || false)  // or
+console.log(!true)  // not
+
+
+// comprehense operation
+const b = 10
+const result = b > 5 ? true : false  // same as A if .. else B
+
+console.log(result)
