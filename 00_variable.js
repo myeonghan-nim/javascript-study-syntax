2f604a6a409e9413cf15ae307d8fe37c4eb2cf7b
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-10-29 12:04:01 +0900

    Add: variables

--- 00_variable.js
+++ 00_variable.js
@@ -49,3 +49,72 @@ function varTest() {
 varTest()
 console.log(x)
 */
+
+// var : define, mutuble free / function-scope (~ES5)
+// let : define once, mutuble free / block-scope
+// const : define once, unmutuble / block-scope
+
+
+// define variables
+// types of variables
+/*
+let dog
+let variableName  // carmelcase
+
+let dogs = []  // define array
+
+function getName() {
+  // define function
+}
+
+const onClick = () => {}  // define event(start with on/off)
+
+let isValid = false  // define boolean(start with is)
+
+class User {
+  // define class(pascalcase)
+  constructor (value) {
+    // define constructor
+    this.name = value.name
+  }
+}
+
+const API_KEY = ''  // unmutuble never
+*/
+
+// number type
+const a = 12
+const b = -5
+const c = 3.14
+const d = 2.9e8
+const e = Infinity  // number type
+const f = -Infinity
+const g = NaN  // complex number prints NaN
+
+console.log(a, b, c, d,typeof e, f,typeof g)  // typeof returns type of variable
+console.log(Math.sqrt(-2))
+
+// string type
+const sentence1 = 'hi\n'
+const sentence2 = "hihi\n"
+const sentence3 = `hi${sentence2}`
+
+console.log(sentence1 + sentence2, sentence3)
+
+// boolean type
+const isValid = true
+
+// non-value type
+let first_name
+
+console.log(typeof first_name)  // undefined(auto), undefined type
+
+let last_name = null
+
+console.log(typeof last_name)  // null, object type
+
+console.log(null == undefined)
+console.log(null === undefined)
+
+console.log(null + 1)  // null can calculate
+console.log(undefined + 1)
