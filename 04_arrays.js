b0bee19a99efa43ce6cfc214b724c9a5fc331c61
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-10-29 15:06:53 +0900

    Add: array

---
+++ 04_arrays.js
@@ -0,0 +1,29 @@
+// array in js
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers[0])
+console.log(numbers.length)
+
+// inner function of array
+console.log(numbers.reverse())  // change original array
+console.log(numbers)
+
+numbers.push(6)
+console.log(numbers)
+
+numbers.pop()
+console.log(numbers)
+
+numbers.unshift(6)
+console.log(numbers)
+
+numbers.shift()
+console.log(numbers)
+
+console.log(numbers.includes(1))
+console.log(numbers.includes(100))
+
+console.log(numbers.indexOf(1))
+console.log(numbers.indexOf(100))  // return -1, when there is not number in array
+
+console.log(numbers.join())
